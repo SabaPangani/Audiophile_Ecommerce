000d1dfbc9e8507556deb5e2733402720258038e
SabaPangani <Spangani37@gmail.com>
2023-02-14 01:50:05 +0400

    fixed mobile nav

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/shared/components/services/services.service';
-
+import {  BreakpointObserver,  BreakpointState} from '@angular/cdk/layout';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
-  constructor(private modalService:ModalService){}
+export class NavComponent implements OnInit {
+  constructor(private modalService:ModalService,public breakpointObserver: BreakpointObserver,){}
   mobileMenu:boolean = false; 
+  showNav:boolean = true;
+  showBurgerMenu = false;
   openCart(){
     this.modalService.openCartDialog();
   }
@@ -16,4 +18,35 @@ export class NavComponent {
   showMobileMenu(){
     this.mobileMenu = !this.mobileMenu;
   }
+
+  ngOnInit(){
+    this.breakpointObserver
+    .observe(['(min-width: 769px)'])
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.showNav = true
+        this.showBurgerMenu = false
+        this.mobileMenu = false;
+      } else {
+        this.showNav = false
+        this.showBurgerMenu = true
+      }
+    });
+  }
+
+  products = [
+    {
+      'imgPath': "https://audiophile-challenge.netlify.app/assets/shared/desktop/image-category-thumbnail-headphones.png",
+      'name': 'headphones',
+    },
+    {
+      'imgPath': "https://audiophile-challenge.netlify.app/assets/shared/desktop/image-category-thumbnail-speakers.png",
+      'name': 'speakers',
+
+    },
+    {
+      'imgPath': "https://audiophile-challenge.netlify.app/assets/shared/desktop/image-category-thumbnail-earphones.png",
+      'name': 'earphones'
+    },
+  ]
 }
